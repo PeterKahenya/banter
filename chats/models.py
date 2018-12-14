452ce4022eac849a339db56c3c569566f46286e7
PeterKahenya <kahenya0@gmail.com>
2018-12-15 01:47:40 +0300

    Added the model Message

---
+++ chats/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Message(models.Model):
+    sender=models.ForeignKey(User,on_delete=models.CASCADE,related_name="messages")
+    receiver=models.ForeignKey(User,on_delete=models.CASCADE)
+    msg=models.TextField(max_length=10000)
+
+    def __str__(self):
+        return str(self.sender)+"   " +str(self.receiver)+"     "+self.msg
